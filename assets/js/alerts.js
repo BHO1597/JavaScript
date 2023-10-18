ea6af57908897c34c7bb91178d231185e0c6d0e0
Brandon Hospina <brahos15@gmail.com>
2023-10-18 12:18:01 -0500

    subiendo 2da seccion completa

---
+++ assets/js/alerts.js
@@ -0,0 +1,23 @@
+// console.log(miNombre);
+
+alert('Hola mundo');
+
+// // lo que esta en parentesis son argumentos o parametros hacia una funcion
+// // null y undefined son diferentes cosas
+let nombre = prompt('¿Cuál es tu nombre?');
+console.log(nombre);
+console.log('****' + nombre + '****');
+
+const seleccion = confirm('¿Estas seguro de borrar esto?');
+console.log(seleccion);
+
+// El objeto window acepta alert / confirm / promp pero no el objeto Node
+
+
+// El objeto Node tiene su objeto Global
+// Es importante saber si sera para front o para backend y saber que objeto usar
+
+// console.log(global);
+
+
+
---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+// console.log(miNuevoNombre + ' Osco');
+
+// alert('Hola desde app.js');
+
+// console.log('Hola mundo');
+
+// console.log (console.log('Hola mundo'));
+
+// console.log('Hola mundo nuevo version 2 con git');
+
+// var b = 20;
+// const c = 10;
+
+// c = 30;
+
+let a = 10, 
+    b = 20 , 
+    c = 'Hola ' , 
+    d = 'Spiderman' , 
+    x = a + b;
+
+const saludo = c + d;
+
+
+// console.log('%c Mis variables', 'color:blue ; font-weight: bold');
+// console.log( { a } );
+// console.log( { b } );
+// console.log( { c } );
+
+c = 'Hola de nuevo';
+
+// console.table({a, b, c, d, x});
+
+// var miNombre = 'Brandon';
+
+// let y const no sobreescriben las variables que se encuentran en el objeto global window 
+// o donde este corriendo
+
+// let outerWidth = 1000000;
+// const outerHeight = 600;
+
+// Con let ejecutamos un codigo mas limpio, los errores se muestran en dialogo mas especificos
+// var usarlo con navegadores viejos
+
+let miNuevoNombre = 'Brandon Hospina';
