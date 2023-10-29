f80dc11eb061b5caa6636c34c920885e365eb21f
Brandon Hospina <brahos15@gmail.com>
2023-10-28 23:42:10 -0500

    Sesion 3 video nro 8

---
+++ assets/js/arreglos.js
@@ -0,0 +1,30 @@
+
+
+// const arr = new ArrayBuffer(10);
+// const arr = [];
+// console.log(arr);
+
+let videoJuegos = [ 'Mario 3', 'Megaman', 'Chrono Trigger' ];
+console.log({ videoJuegos });
+
+console.log( videoJuegos[0] );
+
+let arregloCosas = [
+    true,
+    123,
+    'Brandon',
+    1 + 2,
+    function(){},
+    ()=>{},
+    { a:1 },
+    ['X', 'Megaman', 'Zero', 'Dr. Light', [
+        'Dr.Willy',
+        'Woodman'
+    ]]
+];
+
+// console.log( {arregloCosas} );
+
+// console.log(arregloCosas[2]);
+
+console.log( arregloCosas[7][4][1] );
--- assets/js/palabras-reservadas.js
+++ assets/js/palabras-reservadas.js
@@ -14,7 +14,7 @@
 
 // enum 
 
-
+let _objeto$123 = 123; // No es recomendable esta nomenclatura
 let precio99_99 = 123;
 
 let jugadorConMejorPuntaje = 'Brandon';
