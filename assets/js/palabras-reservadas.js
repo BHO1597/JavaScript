3797da08b4c18be45bf77f97134e02cf51ae68c8
Brandon Hospina <brahos15@gmail.com>
2026-08-19 17:11:15 -0500

    avance palabras reservadas

--- assets/js/palabras-reservadas.js
+++ assets/js/palabras-reservadas.js
@@ -1,3 +1,10 @@
+// Linux case sentive
+
+// palabras reservadas, son palabras que tienen un uso especifico para el cual fueron creadas
+
+// let const = 123; no puedo poner la palabra const xq const es para crear constantes
+
+// let constante = 123; si puedo crearlo porque esta en español y no es una palabra reservada
 
 // console.log('Hola mundo');
 
@@ -12,13 +19,16 @@
 
 // for()
 
-// enum 
+// enum  no cambia de color y es mas dificil de identificar, solo es soportado en TypeScript
 
-let _objeto$123 = 123; // No es recomendable esta nomenclatura
-let precio99_99 = 123;
+// let await = 123; no da error el nav web lo acepta
 
-let jugadorConMejorPuntaje = 'Brandon';
+let _objeto$123 = 123; // Es valido pero No es recomendable esta nomenclatura
+let precio99_99 = 123; // las variables deben tener sentido semantico no poner puntos en las var // _ se usa para separar numeros
 
+let jugadorConMejorPuntaje = 'Brandon'; // Tiene que ser significativo
+
+// existe el upper camel case usado para clases
 class JuegoAnio {
     
 }
