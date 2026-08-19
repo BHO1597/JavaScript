c5f54d5764b2fe2af0dbae6c3e9acb3ad6e5abac
Brandon Hospina <brahos15@gmail.com>
2026-08-19 17:27:43 -0500

    avance arreglos en js

--- assets/js/arreglos.js
+++ assets/js/arreglos.js
@@ -1,7 +1,13 @@
+// Los arreglos son muy parecidos a una lista de informacion, que contienen un grupo de elemntos 
 
+// tenemos una coleccion de juegos puede ser un arreglado xq todos los juegos estan contenidos en un estante
+
+// [] cuando veamos estas llaves cuadras en JS significa que es un arreglo
+
+// Los arreglos en Js empiezan en la posicion 0 , 1 , 2 , ....
 
 // const arr = new ArrayBuffer(10);
-// const arr = [];
+// const arr = []; - tambien se puede inicializar asi un arreglo pero es un arreglo vacio
 // console.log(arr);
 
 let videoJuegos = [ 'Mario 3', 'Megaman', 'Chrono Trigger' ];
